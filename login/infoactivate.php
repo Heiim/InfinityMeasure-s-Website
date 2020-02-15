561bd3ca92959479bf10ef0865af1c0ab2c68222
Lucas Nassif <lucas@nassif.co>
2020-02-15 19:33:15 +0100

    activate et register un peu plus mvc

---
+++ login/infoactivate.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="../quirky.css">
+</head>
+
+<header>
+    <div>
+        <div class="logo">
+            <a href="../home.html"><img src="../images/LogoNoName.png" width=100%x height=100%>
+        </div>
+        <p class="name">quirky()</p>
+    </div>
+</header>
+
+<body class="center">
+    <div class="clearing messagecontainer">
+		<p class="message"><?php echo $message ?></p>
+	</div>
+	<div class="connectbutton"><a class="whitelink" href="index.html">Connexion</a></div>
+</body>
+
+<footer>
+    <ul>
+        <li class="button"><a href="cgu.html">CGU</a></li>
+        <li class="button"><a href="contact.html">Nous contacter</a></li>
+        <li class="button"><a href="faq.html">FAQ</a></li>
+        <li class="button"><a href="forum.html">Forum</a></li>
+    </ul>
+</footer>
+
+</html>
---
+++ login/inforegister.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="../quirky.css">
+</head>
+
+<header>
+    <div>
+        <div class="logo">
+            <a href="../home.html"><img src="../images/LogoNoName.png" width=100%x height=100%></a>
+        </div>
+        <p class="name">quirky()</p>
+    </div>
+</header>
+
+<body>
+    <div class="clearing messagecontainer">
+        <p class="message"><?php echo $messagedisp ?></p>
+    </div>
+</body>
+
+<footer>
+    <ul>
+        <li class="button"><a href="cgu.html">CGU</a></li>
+        <li class="button"><a href="contact.html">Nous contacter</a></li>
+        <li class="button"><a href="faq.html">FAQ</a></li>
+        <li class="button"><a href="forum.html">Forum</a></li>
+    </ul>
+</footer>
+
+</html>
