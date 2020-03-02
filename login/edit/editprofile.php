5f37f06c2ad551b7cd532b89ad7bce6b7fe23617
Lucas Nassif <lucas@nassif.co>
2020-03-02 14:56:33 +0100

    edit profile fonctionnel sauf le mail

---
+++ login/edit/editprofile.php
@@ -0,0 +1,29 @@
+<?php
+
+
+session_start();
+// Si l'utilisateur est pas loggué on le redirige vers la page de login
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: ../index.html');
+	exit();
+}
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'quirky';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// Les infos sont pas stokées dans la session, on les récupère ici
+$stmt = $con->prepare('SELECT password, email, firstn, lastn, birthday, picture FROM accounts WHERE id = ?');
+// On utilise l'id pour récuperer les infos
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($password, $email, $firstn, $lastn, $birthday, $picture);
+$stmt->fetch();
+$stmt->close();
+
+include 'editprofileinfo.php';
+
+?>
---
+++ login/edit/editprofileinfo.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="../../quirky.css">
+        <link rel="icon" type="image/png" href="../../images/infinitelogo.png" />
+    </head>
+
+    <header>
+    <div>
+        <div class="logo">
+            <a href="../../home.html"><img src="../../images/infinitelogo.png" width=100%x height=100%>
+        </div>
+        <p class="name">Infinite Measures</p>
+        </a>
+    </div>
+    <div class="connection">
+        <ul>
+            <li class="button"><a class="whitelink" href="../profile.php">Mon compte</a></li>
+            <li class="button"><a class="whitelink" href="../logout.php">Déconnexion</a></li>
+        </ul>
+    </div>
+</header>
+
+	<body>
+        <div class="clearing registerformcontainer">
+            <div class="registerform">
+                <form action="editsubmit.php" method="post" autocomplete="off">
+                    <div>
+                        <label for="lastn">Nom</label>
+                        <input type="text" name="lastn" id="lastn" placeholder=<?=$lastn?> value=<?=$lastn?> required>
+                    </div>
+                    <div>
+                        <label for="firstn">Prénom </label>
+                        <input type="text" name="firstn" id="firstn" placeholder=<?=$firstn?> value=<?=$firstn?> required>
+                    </div>
+                    <div>
+                        <label for="birthday">Date de naissance </label>
+                        <input type="date" name="birthday" id="birthday" placeholder=<?=$birthday?> value=<?=$birthday?> required>
+                    </div>
+                    <div>
+                        <label for="email">Adresse email </label>
+                        <input type="email" name="email" id="email" placeholder=<?=$email?> value=<?=$email?> required>
+                    </div>
+                    <input type="hidden" name="id" id="id" value=<?=$_SESSION['id']?>>
+                    <div>
+                        <input type="submit" value="Confirmer">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
+    
+<footer>
+    <ul>
+        <li class="button"><a class="whitelink" href="cgu.html">CGU</a></li>
+        <li class="button"><a class="whitelink" href="contact.html">Nous contacter</a></li>
+        <li class="button"><a class="whitelink" href="faq.html">FAQ</a></li>
+        <li class="button"><a class="whitelink" href="forum.html">Forum</a></li>
+    </ul>
+</footer>
+
+</html>
