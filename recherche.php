7e892f387a74c4eecc2e7544adb52e7558ca451a
Heiim <43711510+Heiim@users.noreply.github.com>
2020-03-03 22:32:33 +0100

    Completed CGU, new footer, recherche

---
+++ recherche.php
@@ -0,0 +1,69 @@
+<?php
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'quirky';
+
+$validation=true;
+
+if(isset($_POST['nom'])) {
+	$nom_user .= "firstn=".$_POST['nom'];
+}else {
+	$nom_user .= "firstn "."IS NOT NULL";
+}
+
+
+if(isset($_POST['prenom'])) {
+	$prenom_user .= "lastn=".$_POST['prenom'];
+}else {
+	$prenom_user .= "lastn "."IS NOT NULL";
+}
+
+
+
+if($_POST['status'] >0) {
+	$status_user .= "status=".$_POST['nom'];
+}else {
+	$status_user .= "status "."IS NOT NULL";
+}
+
+
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$bdd = mysql_select_db("accounts")
+if (mysqli_connect_errno()) {
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+$user = $bdd->prepare('SELECT id, firstn, lastn, status FROM accounts WHERE ? AND ? AND ? ORDER BY id DESC');
+$user->bind_param('sss', $nom_user, $prenom_user, $status_user)
+$user->execute();
+if($user->rowCount() != 0) {
+	$user = $bddprepare('SELECT id, firstn, lastn, status FROM accounts WHERE ? OR ? OR ? ORDER BY id DESC');
+	$user->bind_param('sss', $nom_user, $prenom_user, $status_user)
+	$user->execute();
+}
+
+?>
+
+<form method="GET">
+	<input type="search" name="nom" placeholder="Nom" />
+	<input type="search" name="prenom" placeholder="Prénom" />
+	<select name="status" placeholder="Status">
+		<option value="0"></option>
+		<option value="User">Utilisateur</option>
+		<option value="Gestionnaire">Gestionnaire</option>
+		<option value="Admin">Administrateur</option>
+	</select>
+	<input type="submit" value="Chercher" />
+<?php if($user->rowCount() > 0) { ?>
+   <ul>
+   <?php while($a = $user->fetch()) { ?>
+      <li><?= $a ?></li>
+   <?php } ?>
+   </ul>
+<?php } else { ?>
+Aucun résultat pour: <?= $_POST['nom']." ".$_POST['prenom']." ".$_POST['status'] ?>...
+<?php } ?>
+</form>
+
