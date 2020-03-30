c293f853171fcdb62f5fef9024e6a4382a90c8f2
Heiim <43711510+Heiim@users.noreply.github.com>
2020-03-30 12:37:10 +0200

    +gender

--- barrerecherche/index.php
+++ barrerecherche/index.php
@@ -23,22 +23,22 @@ if(isset($_GET['nom']) AND !empty($_GET['nom'])) {
 
 if(isset($_GET['status']) AND !empty($_GET['status'])) {
     $status = htmlspecialchars($_GET['status']);
-    $status_user = "status "."IS NOT NULL";
+    $status_user = $status;
 }else{
-    $status_user = "idaccount "."IS NOT NULL";
+    $status_user = "%";
 }
 
 if(isset($_GET['gender']) AND !empty($_GET['gender'])) {
     $gender = htmlspecialchars($_GET['gender']);
-    $gender_user = "gender = "."'$gender'";
+    $gender_user = $gender;
 }else{
-    $gender_user = "gender "."IS NOT NULL";
+    $gender_user = "%";
 }
 
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-$stmt = $con->prepare("SELECT idaccount, firstn, lastn, email, idadmin, idmanager FROM accounts LEFT JOIN users ON accounts.idaccount = users.iduser LEFT JOIN managers ON accounts.idaccount = managers.idmanager LEFT JOIN admins ON accounts.idaccount = admins.idadmin WHERE firstn LIKE ? AND lastn LIKE ?");
-$stmt->bind_param('ss',$prenom_user, $nom_user);
+$stmt = $con->prepare("SELECT idaccount, firstn, lastn, email, idadmin, idmanager FROM accounts LEFT JOIN users ON accounts.idaccount = users.iduser LEFT JOIN managers ON accounts.idaccount = managers.idmanager LEFT JOIN admins ON accounts.idaccount = admins.idadmin WHERE firstn LIKE ? AND lastn LIKE ? AND gender LIKE ?");
+$stmt->bind_param('sss',$prenom_user, $nom_user, $gender_user);
 $stmt->execute();
 $stmt->bind_result($idaccount, $firstn, $lastn, $email, $idadmin, $idmanager);
 
