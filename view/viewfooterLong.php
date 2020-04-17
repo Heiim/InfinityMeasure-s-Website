50cec3355fcea2f808a9b69ad7fac950e1af5b76
Heiim <43711510+Heiim@users.noreply.github.com>
2020-04-17 23:39:46 +0200

    justement footer v2

--- view/viewfooterLong.php
+++ view/viewfooterLong.php
@@ -1,3 +1,5 @@
+<?php
+echo"
 <div class="footerLong">
     <ul>
         <li class="button"><a class="whitelink" href="cgu.html">CGU</a></li>
@@ -5,4 +7,6 @@
         <li class="button"><a class="whitelink" href="faq.html">FAQ</a></li>
         <li class="button"><a class="whitelink" href="forum.html">Forum</a></li>
     </ul>
- </div>
+ </div>";
+
+ ?>
