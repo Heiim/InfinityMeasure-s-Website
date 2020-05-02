73fadb6ef3517166668189c5ced8da55136e6ba8
YEANG VINCENT <vincentyeang@gmail.com>
2020-05-03 00:40:12 +0200

    Page nous contacter fonctionnelle

--- model/postcontact.php
+++ model/postcontact.php
@@ -5,7 +5,7 @@ $subject = 'Formulaire de contact';
 $headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
 $message = 'Subject :' . "\r\n" . $_POST["subject"] . '<br/>' . 'Last name :' . "\r\n" . $_POST["lastn"] . '<br/>' . 'First name :' . "\r\n" . $_POST["firstn"] . '<br/>' .'email:' . "\r\n" . $_POST["email"] . '<br/>' . $contain;
 
-mail("vincentyeang@gmail.com",$subject, $message, $headers);
+mail("lucas.nassif@isep.fr",$subject, $message, $headers);
 header('Location: index.php?action=contactussend');
 
 ?>
