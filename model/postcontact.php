af85cc5bb8732a204589ed59e43e4212ed6edea4
YEANG VINCENT <vincentyeang@gmail.com>
2020-05-03 17:54:35 +0200

    Page nous contacter fini

--- model/postcontact.php
+++ model/postcontact.php
@@ -1,11 +1,18 @@
 <?php 
+require('model/connectdb.php');
+$stmt = $con->prepare("SELECT email FROM admins LEFT JOIN accounts ON admins.idadmin = accounts.idaccount=1");
+$stmt->execute();
+$stmt->bind_result($email);
+$stmt->fetch();
+$stmt->close();
+
 $contain=nl2br(htmlentities($_POST["contain"]));
 $from    = 'quirkylimited@gmail.com';
 $subject = 'Formulaire de contact';
 $headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
 $message = 'Subject :' . "\r\n" . $_POST["subject"] . '<br/>' . 'Last name :' . "\r\n" . $_POST["lastn"] . '<br/>' . 'First name :' . "\r\n" . $_POST["firstn"] . '<br/>' .'email:' . "\r\n" . $_POST["email"] . '<br/>' . $contain;
 
-mail("lucas.nassif@isep.fr",$subject, $message, $headers);
+mail($email,$subject, $message, $headers);
 header('Location: index.php?action=contactussend');
 
 ?>
