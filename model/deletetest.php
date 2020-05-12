c480377454a4a7d1488380be39e0c19dcedf030b
Lucas Nassif <lucas@nassif.co>
2020-05-12 17:17:51 +0200

    delete to update

--- model/deletetest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('model/connectdb.php');
-
-
-$stmt = $con->prepare('DELETE FROM tests WHERE idtest = ?');
-$stmt->bind_param('i', intval($_GET['id']));
-$stmt->execute();
-$stmt->close();
