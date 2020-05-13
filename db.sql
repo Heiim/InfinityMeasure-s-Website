38e1b7feac923538642fd477490b3e4ca85a4405
Lucas Nassif <lucas@nassif.co>
2020-05-13 17:06:20 +0200

    ajout de la db

---
+++ db.sql
@@ -0,0 +1,444 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 13 mai 2020 à 17:03
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `quirky`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `idaccount` int(11) NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `activation_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `firstn` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastn` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `picture` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'public/images/default.png',
+  `token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `accounts`
+--
+
+INSERT INTO `accounts` (`idaccount`, `password`, `email`, `activation_code`, `firstn`, `lastn`, `picture`, `token`) VALUES
+(1, '$2y$10$PqSYN00b/D7GcTLInrSr..khzi77iZd.FuEQgaiF1xQwFnMV2KFXS', 'admin', 'activated', 'Admin', 'Default', 'public/images/default.png', '5ebc0a32ed7c9');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admins`
+--
+
+CREATE TABLE `admins` (
+  `idadmin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `admins`
+--
+
+INSERT INTO `admins` (`idadmin`) VALUES
+(1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admintokens`
+--
+
+CREATE TABLE `admintokens` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `companies`
+--
+
+CREATE TABLE `companies` (
+  `idcompany` int(11) NOT NULL,
+  `company_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `managers`
+--
+
+CREATE TABLE `managers` (
+  `idmanager` int(11) NOT NULL,
+  `idcompany` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `managertokens`
+--
+
+CREATE TABLE `managertokens` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+CREATE TABLE `messages` (
+  `idmessage` int(11) NOT NULL,
+  `idsender` int(11) DEFAULT NULL,
+  `idreceiver` int(11) DEFAULT NULL,
+  `content` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `posts`
+--
+
+CREATE TABLE `posts` (
+  `idpost` int(11) NOT NULL,
+  `idtopic` int(11) DEFAULT NULL,
+  `idaccount` int(11) DEFAULT NULL,
+  `content` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `results`
+--
+
+CREATE TABLE `results` (
+  `idresults` int(11) NOT NULL,
+  `idtest` int(11) DEFAULT NULL,
+  `iduser` int(11) DEFAULT NULL,
+  `result` int(11) DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sensors`
+--
+
+CREATE TABLE `sensors` (
+  `idsensor` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `sensors`
+--
+
+INSERT INTO `sensors` (`idsensor`, `name`, `description`) VALUES
+(1, 'button', 'Permet d\'effectuer les tests de temps de réaction'),
+(2, 'microphone', 'Permet d\'effectuer les tests de reconnaissance de tonalité'),
+(3, 'temperature', 'Permet de mesurer la température superficielle de la peau'),
+(4, 'phototransistor', 'Permet d\'effectuer le test de fréquence cardiaque');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tests`
+--
+
+CREATE TABLE `tests` (
+  `idtest` int(11) NOT NULL,
+  `idsensor` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `min` int(11) DEFAULT NULL,
+  `max` int(11) DEFAULT NULL,
+  `unit` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `tests`
+--
+
+INSERT INTO `tests` (`idtest`, `idsensor`, `name`, `description`, `min`, `max`, `unit`) VALUES
+(1, 1, 'soundexp', 'Temps de réaction à un son attendu', 0, 450, 'ms'),
+(2, 1, 'soundunexp', 'Temps de réaction à un son inattendu', 0, 450, 'ms'),
+(3, 1, 'lightexp', 'Temps de réaction à une lumière attendue', 0, 450, 'ms'),
+(4, 1, 'lightunexp', 'Temps de réaction à une lumière inattendue', 0, 450, 'ms'),
+(5, 2, 'soundfrequency', 'Reconnaissance de tonalité', 0, 10, 'points'),
+(6, 3, 'temperature', 'Mesure de la température superficielle de la peau', 0, 45, '°C'),
+(7, 4, 'heartfrequency', 'Mesure de la fréquence cardiaque', 0, 180, 'bpm');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `topics`
+--
+
+CREATE TABLE `topics` (
+  `idtopic` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  `status` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'open'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `iduser` int(11) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `gender` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `height` double DEFAULT NULL,
+  `weight` double DEFAULT NULL,
+  `idcompany` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`idaccount`);
+
+--
+-- Index pour la table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`idadmin`);
+
+--
+-- Index pour la table `admintokens`
+--
+ALTER TABLE `admintokens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `companies`
+--
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`idcompany`);
+
+--
+-- Index pour la table `managers`
+--
+ALTER TABLE `managers`
+  ADD PRIMARY KEY (`idmanager`),
+  ADD KEY `idcompany` (`idcompany`);
+
+--
+-- Index pour la table `managertokens`
+--
+ALTER TABLE `managertokens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`idmessage`),
+  ADD KEY `idsender` (`idsender`),
+  ADD KEY `idreceiver` (`idreceiver`);
+
+--
+-- Index pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`idpost`),
+  ADD KEY `idtopic` (`idtopic`),
+  ADD KEY `idaccount` (`idaccount`);
+
+--
+-- Index pour la table `results`
+--
+ALTER TABLE `results`
+  ADD PRIMARY KEY (`idresults`),
+  ADD KEY `idtest` (`idtest`),
+  ADD KEY `iduser` (`iduser`);
+
+--
+-- Index pour la table `sensors`
+--
+ALTER TABLE `sensors`
+  ADD PRIMARY KEY (`idsensor`);
+
+--
+-- Index pour la table `tests`
+--
+ALTER TABLE `tests`
+  ADD PRIMARY KEY (`idtest`),
+  ADD KEY `idsensor` (`idsensor`);
+
+--
+-- Index pour la table `topics`
+--
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`idtopic`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`iduser`),
+  ADD KEY `idcompany` (`idcompany`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `idaccount` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `admintokens`
+--
+ALTER TABLE `admintokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `idcompany` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `managertokens`
+--
+ALTER TABLE `managertokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `idmessage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `idpost` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `results`
+--
+ALTER TABLE `results`
+  MODIFY `idresults` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `sensors`
+--
+ALTER TABLE `sensors`
+  MODIFY `idsensor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `tests`
+--
+ALTER TABLE `tests`
+  MODIFY `idtest` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `topics`
+--
+ALTER TABLE `topics`
+  MODIFY `idtopic` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `admins`
+--
+ALTER TABLE `admins`
+  ADD CONSTRAINT `admins_ibfk_1` FOREIGN KEY (`idadmin`) REFERENCES `accounts` (`idaccount`);
+
+--
+-- Contraintes pour la table `managers`
+--
+ALTER TABLE `managers`
+  ADD CONSTRAINT `managers_ibfk_1` FOREIGN KEY (`idcompany`) REFERENCES `companies` (`idcompany`),
+  ADD CONSTRAINT `managers_ibfk_2` FOREIGN KEY (`idmanager`) REFERENCES `accounts` (`idaccount`);
+
+--
+-- Contraintes pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`idsender`) REFERENCES `accounts` (`idaccount`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`idreceiver`) REFERENCES `accounts` (`idaccount`);
+
+--
+-- Contraintes pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`idtopic`) REFERENCES `topics` (`idtopic`),
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`idaccount`) REFERENCES `accounts` (`idaccount`);
+
+--
+-- Contraintes pour la table `results`
+--
+ALTER TABLE `results`
+  ADD CONSTRAINT `results_ibfk_1` FOREIGN KEY (`idtest`) REFERENCES `tests` (`idtest`),
+  ADD CONSTRAINT `results_ibfk_2` FOREIGN KEY (`iduser`) REFERENCES `users` (`iduser`);
+
+--
+-- Contraintes pour la table `tests`
+--
+ALTER TABLE `tests`
+  ADD CONSTRAINT `tests_ibfk_1` FOREIGN KEY (`idsensor`) REFERENCES `sensors` (`idsensor`);
+
+--
+-- Contraintes pour la table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`idcompany`) REFERENCES `companies` (`idcompany`),
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`iduser`) REFERENCES `accounts` (`idaccount`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
