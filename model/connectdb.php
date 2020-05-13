83833b291e18e77b2fe10dc283b0901c6c32d8e5
Lucas Nassif <lucas@nassif.co>
2020-05-13 16:31:39 +0200

    quick fix

--- model/connectdb.php
+++ model/connectdb.php
@@ -1,10 +1,10 @@
 <?php
 
 //info de connexion
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'id13557950_db_admin_qr4896';
-$DATABASE_PASS = 'uMD7g4*FiVPnJs^oVFGw1';
-$DATABASE_NAME = 'id13557950_quirky';
+$DATABASE_HOST = '';
+$DATABASE_USER = '';
+$DATABASE_PASS = '';
+$DATABASE_NAME = '';
 
 //connexion a la DB
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
